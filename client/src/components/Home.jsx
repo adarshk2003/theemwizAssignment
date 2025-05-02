21e7b98e9eeaedd75a6606d41e9cb9b3dcf29eee
adarsh <reaemp4@gmailcom>
2025-05-02 18:44:48 +0100

    updated

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import NavbarComponent from "./sections/NavBar"
-import Section1 from "./sections/section1"
+import Section1 from "./sections/Section1"
 import Section2 from "./sections/Section2";
 import Section3 from "./sections/Section3";
 export default function Home() {
